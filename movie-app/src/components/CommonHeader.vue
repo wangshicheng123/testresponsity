f1e5f639656071e4a8901e7abfb3e7841622b920
wangshicheng123 <3137252538@qq.com>
2019-04-27 10:33:02 +0800

    测试 vuecli3

---
+++ movie-app/src/components/CommonHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <span>首页</span>
+            <span>电影</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+<style>
+
+</style>
