398fa3436cdbbf00adb7c96f2d97cbaf19a046a1
wangshicheng123 <3137252538@qq.com>
2019-05-11 20:10:59 +0800

    项目的初始化

---
+++ vuemyapp/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <img src="@/assets/logo.png" alt="">
+    
+    <div class="title">这是一个标题</div>
+  </div>
+</template>
+
+<style>
+  .title{
+    font-size: 1rem;
+  }
+</style>
