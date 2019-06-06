ff45fc0b9a0da98e96e8b22bf66be3e8dc7ae379
wangshicheng123 <3137252538@qq.com>
2019-06-06 21:28:22 +0800

    test

---
+++ 初步node/test12.js
@@ -0,0 +1,15 @@
+// <meta charset="utf-8">
+
+// 返回图片；
+//content-type: image/jpeg
+// 解析query字符串
+// querystring.parse(querystr);
+// querystring.stringfy(obj);
+
+
+// 当发送post请求的时候，
+// 需要监听req的事件
+// 做数据流的时候：
+// req.setEncoding("utf8");
+
+
