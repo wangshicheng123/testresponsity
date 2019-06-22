8d4531a05d32e600871d04f93ae5c0555a57ce6c
wangshicheng123 <3137252538@qq.com>
2019-06-22 10:41:44 +0800

    node

--- 初步node/newblog/routes/index.js
+++ 初步node/newblog/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("reg",function(req,res,next){
+  res.render("reg");
+});
+
 module.exports = router;
